1b600d20b5a7e14368fd48e0ca0b75eabbcf4aa4
unknown <tonderaimuchada@gmail.com>
2025-10-20 10:00:09 +0200

    Added customer and product controller skeleton together with packages templates

---
+++ src/main/java/za/co/backspace/controller/ProductController.java
@@ -0,0 +1,10 @@
+package za.co.backspace.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Tonderai Muchada on 20/10/2025 - 9:56 AM
+ */
+@RestController
+public class ProductController {
+}

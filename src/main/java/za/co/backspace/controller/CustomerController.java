cb1ab57dec3b64315a540a3fb0b18ffd6114ddde
unknown <tonderaimuchada@gmail.com>
2025-10-20 11:36:18 +0200

    Added methods for customer controller

--- src/main/java/za/co/backspace/controller/CustomerController.java
+++ src/main/java/za/co/backspace/controller/CustomerController.java
@@ -1,9 +1,9 @@
 package za.co.backspace.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
 import za.co.backspace.entity.Customer;
+import za.co.backspace.entity.Order;
 
 import java.util.List;
 
@@ -17,4 +17,35 @@ public class CustomerController {
     public List<Customer> getCustomers(){
         return null;
     }
+
+    @PostMapping
+    public Customer addCustomer(@RequestBody Customer customer){
+        return null;
+    }
+
+    @PutMapping
+    public Customer updateCustomer(@RequestBody Customer customer){
+        return null;
+    }
+
+    @DeleteMapping
+    public void deleteCustomer(@RequestBody Customer customer){}
+
+    @GetMapping
+    public List<Order> getOrdersForCustomer(@RequestBody Customer customer){
+        return null;
+    }
+
+    @PostMapping
+    public Order addOrder(@RequestBody Order order, Integer customerId){
+        return null;
+    }
+
+    @PutMapping
+    public Order updateOrder(@RequestBody Order order){
+        return null;
+    }
+
+    @DeleteMapping
+    public void deleteOrder(@RequestBody Order order){}
 }

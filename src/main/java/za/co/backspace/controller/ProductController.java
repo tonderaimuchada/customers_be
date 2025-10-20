e8246e884f3f51bf12773d74a8a7762a928a4f26
unknown <tonderaimuchada@gmail.com>
2025-10-20 11:37:50 +0200

    Added skeleton methods for product controller

--- src/main/java/za/co/backspace/controller/ProductController.java
+++ src/main/java/za/co/backspace/controller/ProductController.java
@@ -1,10 +1,30 @@
 package za.co.backspace.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import za.co.backspace.entity.Product;
+
+import java.util.List;
 
 /**
  * @author Tonderai Muchada on 20/10/2025 - 9:56 AM
  */
 @RestController
 public class ProductController {
+    @GetMapping
+    public List<Product> getProducts(){
+        return null;
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product){
+        return null;
+    }
+
+    @PutMapping
+    public Product updateProduct(@RequestBody Product product){
+        return null;
+    }
+
+    @DeleteMapping
+    public void deleteProduct(@RequestBody Product product){}
 }

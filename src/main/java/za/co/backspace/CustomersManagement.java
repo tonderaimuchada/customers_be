4e9343aff4d28181e9075d58e73b7c9dc44b772e
unknown <tonderaimuchada@gmail.com>
2025-10-22 00:58:53 +0200

    Configured application context and h2 database settings

---
+++ src/main/java/za/co/backspace/CustomersManagement.java
@@ -0,0 +1,9 @@
+package za.co.backspace;
+
+import org.springframework.boot.SpringApplication;
+
+public class CustomersManagement {
+    static void main(String[] args) {
+        SpringApplication.run(CustomersManagement.class, args);
+    }
+}
